f26252b90aa7bc6997bb08732d9e1d8f577cfcf9
Kristin Brooks <kristin.ruth.brooks@gmail.com>
2019-07-21 19:44:00 -0700

    added some commentsk

--- src/CIS129_KristinBrooks_PC7_1b.java
+++ src/CIS129_KristinBrooks_PC7_1b.java
@@ -10,24 +10,30 @@
  ****************************************************************/
 
 public class CIS129_KristinBrooks_PC7_1b {
-
+    
     public static void main(String[] args) {
+        // create instances of the new class
+        // one is declared with it's data, the other two are given data below with 'set' functions
         CIS129_KristinBrooks_PC7_1a kristin = new CIS129_KristinBrooks_PC7_1a("Kristin Brooks",
                 "1931 N Tucker Drive,", "Tucson", "AZ", "85916", 37,
                 "5039614774");
         CIS129_KristinBrooks_PC7_1a chad = new CIS129_KristinBrooks_PC7_1a();
         CIS129_KristinBrooks_PC7_1a kt = new CIS129_KristinBrooks_PC7_1a();
 
+        // setting Chad's info
         chad.setName("Chad Woolley");
         chad.setAddress("1234 East 1st Street", "Tucson", "AZ", "12345");
         chad.setAge(47);
         chad.setPhoneNumber("5201234567");
 
+        // setting KT's info
         kt.setName("Katie Wylie");
         kt.setAddress("456 West 1st Street", "Louisville", "KY", "54321");
         kt.setAge(37);
         kt.setPhoneNumber("5021234567");
 
+        // printing out everyone's info using the 'get' functions
+        // this will verify the class is working as designed
         System.out.println(kristin.getName() + "\n" + kristin.getAddress() + "\n" +
                 kristin.getAge() + kristin.getPhoneNumber());
         System.out.println();
